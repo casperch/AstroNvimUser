9c97a2cef4e85e59f2c0a4c3e24d10d2537deaba
Jae-Pil Choi <casperch@gmail.com>
2024-11-18 14:56:27 +0900

    user plugin merge

--- init.lua
+++ init.lua
@@ -22,3 +22,5 @@ require "os"
 vim.g.python3_host_prog = os.getenv("HOME") .. "/.pyenv/shims/python3"
 vim.g.loaded_perl_provider = 0
 vim.g.loaded_ruby_provider = 0
+
+vim.opt.makeprg = "cmake -B build . && cmake --build build"
